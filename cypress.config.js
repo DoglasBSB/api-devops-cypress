d87ac2214ca9fa14eae94ff13418e50f55cc49fb
Dôglas <doglas@pop-os.localdomain>
2024-04-02 20:29:08 -0300

    project api

---
+++ cypress.config.js
@@ -0,0 +1,11 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  projectId: "dzq1cc",
+  e2e: {
+    baseUrl: 'https://api.restful-api.dev',
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+});
